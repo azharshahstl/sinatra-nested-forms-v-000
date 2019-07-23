18713de6fb78577ac046c755b0633ae8ffdf0960
Azhar Shah <azharshahstl@yahoo.com>
2019-07-23 23:36:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,9 +18,9 @@ module FormsLab
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
-      
+    
       @ships = Ship.all
-      
+
       erb :'pirates/show' 
       
     end
